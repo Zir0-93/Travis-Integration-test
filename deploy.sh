b4b7dfea532e1351dede9c6c32d4e185596d69ee
k-dang <dangkevin12@gmail.com>
2016-08-08 00:35:47 -0400

    random EOF

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [[ $TRAVIS_EVENT_TYPE == 'push' ]]; then
 	if [[ $TRAVIS_BRANCH == 'master' ]]; then
-		echo "ok to deploy"
+		echo "ok to deploy!"
 		curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"  |   tar -zx
 		export PATH=$PATH:.
 		cf --version
